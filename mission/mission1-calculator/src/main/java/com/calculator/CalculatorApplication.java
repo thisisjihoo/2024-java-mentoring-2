61e30da59b1c1e020f563e76fafcd5c8dc98b4ea
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2025-01-18 18:06:27 +0900

    refactoring: 생성자 주입으로 변경

--- mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
+++ mission/mission1-calculator/src/main/java/com/calculator/CalculatorApplication.java
@@ -1,11 +1,15 @@
 package com.calculator;
 
 import com.calculator.controller.CalculatorController;
+import com.calculator.domain.StringCalculator;
+import com.calculator.view.InputView;
+import com.calculator.view.OutputView;
 
 public class CalculatorApplication {
+
     public static void main(String[] args) {
 
-        CalculatorController calculatorController = new CalculatorController();
+        CalculatorController calculatorController = new CalculatorController(new StringCalculator(), new InputView(), new OutputView());
 
         calculatorController.calculatorControl();
     }
