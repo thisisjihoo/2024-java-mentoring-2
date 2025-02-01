e6b3494b53a2ca5460bfdb7b047133358adb593d
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2025-02-02 01:16:38 +0900

    refactoring: 와일드 카드 사용 수정

--- mission/mission1-calculator/src/test/java/com/calculator/domain/StringCalculatorTest.java
+++ mission/mission1-calculator/src/test/java/com/calculator/domain/StringCalculatorTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class StringCalculatorTest {
 
