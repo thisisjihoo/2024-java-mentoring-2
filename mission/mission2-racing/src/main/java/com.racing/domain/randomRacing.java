7057358ab37c33e376f8033d518e53a4621f51d4
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2025-02-06 15:40:33 +0900

    feat: 랜덤값 확인 후 카운트

--- mission/mission2-racing/src/main/java/com.racing/domain/randomRacing.java
+++ mission/mission2-racing/src/main/java/com.racing/domain/randomRacing.java
@@ -3,10 +3,20 @@ package com.racing.domain;
 import java.util.Random;
 
 public class randomRacing {
+
     public int chooseRandom(){
 
         Random random = new Random();
         return random.nextInt(9);
     }
 
+    public int checkRandomValue(int random){
+
+        int count = 0;
+
+        if(random > 3){
+            count += 1;
+        }
+        return count;
+    }
 }
