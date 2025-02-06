b233e39c332c13a6295ec7f80d7e3880b0af3dc5
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2025-02-07 02:04:10 +0900

    refactoring: 출력문 영어로 변경

--- mission/mission2-racing/src/main/java/com/racing/domain/domain/IllegalIndexException.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/domain/IllegalIndexException.java
@@ -3,6 +3,6 @@ package com.racing.domain.domain;
 public class IllegalIndexException extends IndexOutOfBoundsException {
 
     public IllegalIndexException(){
-        super("범위를 벗어났습니다.");
+        super("Out of range.");
     }
 }
