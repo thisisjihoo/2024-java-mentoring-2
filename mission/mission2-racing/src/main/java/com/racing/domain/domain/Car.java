877b3966a4ee60253bcac01d28172d4e004e2c80
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2025-02-07 23:04:22 +0900

    refactoring: 상수화

--- mission/mission2-racing/src/main/java/com/racing/domain/domain/Car.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/domain/Car.java
@@ -1,10 +1,14 @@
 package com.racing.domain.domain;
 
 import com.racing.domain.domain.vo.RandomValue;
+import com.racing.domain.exception.IllegalIndexException;
 
 public class Car {
 
     private final RandomValue randomValue;
+    private final int NEG_VAL = 0;
+    private final int SPEC_RANG = 3;
+    private final int OUT_RANG = 9;
     private int count = 0;
 
     public Car(RandomValue randomValue) {
@@ -12,27 +16,27 @@ public class Car {
     }
 
     public int RandomNumber() {
-        int number = randomValue.setRandomValue();
+        int number = randomValue.randomValue();
         NegativeValueException(number);
         OutOfRangeException(number);
         return number;
     }
 
     private void NegativeValueException(int number){
-        if (number < 0) {
+        if (number < NEG_VAL) {
             throw new IllegalIndexException();
         }
     }
 
     private void OutOfRangeException(int number){
-        if(number > 9){
+        if(number > OUT_RANG){
             throw new IllegalIndexException();
         }
     }
 
 
     public int checkRandomValue(int random) {
-        if (random > 3) {
+        if (random > SPEC_RANG) {
             count++;
         }
         return count;
