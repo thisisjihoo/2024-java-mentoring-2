d6e3a40d3cae69b6e8762ff9daa0323997d6a1fe
박지후 <105189817+thisisjihoo@users.noreply.github.com>
2025-02-07 18:04:50 +0900

    refactoring: 스캐너 생성자 주입으로 변경

--- mission/mission2-racing/src/main/java/com/racing/domain/RacingApplication.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/RacingApplication.java
@@ -7,18 +7,21 @@ import com.racing.domain.view.InputView;
 import com.racing.domain.view.OutputView;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class RacingApplication {
     private final RandomValue randomValue;
     private final Random random;
+    private final Scanner scanner;
 
-    public RacingApplication(RandomValue randomValue, Random random) {
+    public RacingApplication(RandomValue randomValue, Random random, Scanner scanner) {
         this.randomValue = randomValue;
         this.random = random;
+        this.scanner = scanner;
     }
 
     public static void main(String[] args) {
-        RacingController racingController = new RacingController(new Car(new RandomValue(new Random())), new InputView(), new OutputView());
+        RacingController racingController = new RacingController(new Car(new RandomValue(new Random())), new InputView(new Scanner(System.in)), new OutputView());
         racingController.Racing();
     }
 }
--- mission/mission2-racing/src/main/java/com/racing/domain/view/InputView.java
+++ mission/mission2-racing/src/main/java/com/racing/domain/view/InputView.java
@@ -3,10 +3,14 @@ package com.racing.domain.view;
 import java.util.Scanner;
 
 public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     public String inputCarName(){
-
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("CarName: ");
         return scanner.next();
     }
